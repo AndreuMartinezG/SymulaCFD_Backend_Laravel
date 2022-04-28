dc8d3053c76463cd218ddbba6c970e1fa4ca49c1
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-04-28 14:51:57 +0200

    feat: creado CRUD basico de AuthController

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,15 @@ use App\Http\Controllers\AuthController;
 
 
 
+// CRUD AUTH y USERS
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group([
+    'middleware' => 'jwt.auth'
+], function () {
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+});

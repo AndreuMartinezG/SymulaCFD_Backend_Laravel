c1dca22643fe899d58c43d4739137578a9636eba
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-15 23:43:44 +0200

    feat: folder for img db and fix error  SQLSTATE[42000]

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,9 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            
         });
+        
     }
 
     /**
